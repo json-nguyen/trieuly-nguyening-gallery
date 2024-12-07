ca3c168c9f125c1cfb72898fb0733831e653a18a
json-nguyen <39976366+json-nguyen@users.noreply.github.com>
2024-12-07 13:04:49 -0500

    created styles folder and added video icon for videos

--- client/src/pages/GalleryPage.jsx
+++ client/src/pages/GalleryPage.jsx
@@ -7,7 +7,8 @@ import { storage } from "../services/firebase";
 import ContentCarousel from "../components/Gallery/ContentCarousel";
 import JBLoader from "../components/shared/JBLoader";
 import InfiniteScroll from "react-infinite-scroll-component";
-import "./GalleryPage.css"
+import "./styles/GalleryPage.css"
+import { PlayCircleIcon } from "@heroicons/react/24/outline";
 const PHOTOS_PER_PAGE = 24
 
 const GalleryPage = () => {
@@ -62,7 +63,6 @@ const GalleryPage = () => {
 
 			const startIndex = page * PHOTOS_PER_PAGE; 
       const items = sortedItems.slice(startIndex, startIndex + PHOTOS_PER_PAGE); 
-			console.log(`$Fetching ${items.length} at index ${startIndex}`)
 			if (items.length === 0) {
         setAllLoaded(true); 
         return;
@@ -134,17 +134,16 @@ const GalleryPage = () => {
 	return (
 		<div className="bg-gray-100 min-h-screen p-4 flex flex-col" ref={containerRef} >	
 			<InfiniteScroll
-        dataLength={photos.length} // Current photos count
-        next={handleLoadMore} // Function to fetch more photos
-        hasMore={!allLoaded} // Check if more photos are available
+        dataLength={photos.length} 
+        next={handleLoadMore}
+        hasMore={!allLoaded}
         loader={
 					<div className="flex justify-center items-center">
 						<JBLoader />
 					</div>
-				} // Loading indicator
-        endMessage={<p style={{ textAlign: "center" }}>You have seen it all!</p>} // End message
+				} 
+        endMessage={<p style={{ textAlign: "center" }}>You have seen it all!</p>} 
       >
-				{/* Album Scroll Container */}
 				<div className="flex overflow-x-auto space-x-4 w-full py-4 items-center">
 					{albums.map((album) => (
 						<AlbumBox
@@ -163,15 +162,21 @@ const GalleryPage = () => {
 						onFileUpload={handleFileUpload}
 					/>
 				</div>
-				  {	loading && photos.length === 0 ? ( // Show the loader only for the initial load or album switch
+				  {	loading && photos.length === 0 ? ( 
 					<div className="flex justify-center items-center flex-grow">
 						<JBLoader />
 					</div> 
 					) : (
+						// PHOTO GLALERY HERE 
 					<div className="photo-grid" style={{ gridTemplateColumns: `repeat(${getColumns()}, 1fr)` }}>
 						{photos.map((photo, index) => (
 							<div key={index} className="photo-item" onClick={() => openModal(index)}>
-								<img src={photo.src} alt={photo.title} className="w-full h-auto" />
+								<div className="relative">
+									<img src={photo.src} alt={photo.title} className="w-full h-auto" />
+									{photo.type === "video" && (
+										<PlayCircleIcon className="absolute top-1 right-1 w-5 h-5 sm:w-7 sm:h-7 md:w-9 md:h-9 text-white" />
+									)}
+								</div>
 							</div>
 						))}
 					</div>
